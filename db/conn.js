71507d3c2774d8d806052e496deb235d0ef39b42
Marco Poetry Chen <marcopchen@gmail.com>
2018-06-02 11:39:59 -0400

    clear repo

--- db/conn.js
+++
@@ -1,9 +0,0 @@
-const Sequelize = require('sequelize');
-const conn = new Sequelize(
-  process.env.DATABASE_URL || 'postgres://localhost/capstone',
-  {
-    logging: false
-  }
-);
-
-module.exports = conn;
--- server.js
+++
@@ -1,4 +0,0 @@
-const app = require('./server/app');
-
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Listening on port ${port}`));

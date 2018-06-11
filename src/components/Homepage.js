10bd2952ccebc6e906d15c67dbd86178db149f16
Marco Poetry Chen <marcopchen@gmail.com>
2018-06-11 11:27:27 -0400

    uninstalled packages for linting

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,44 +1,25 @@
 import React from "react"
 import { connect } from "react-redux"
-import { Signup } from "./SignUpModal"
 
-const Homepage = props => (
+const Homepage = () => (
   <div>
-    {props.isLoggedIn ? (
-      <div className="home" id="background">
-        <div id="homeText">
-          <div className="f1" id="textSignedIn">
-            Aimed at local communities,<br />
-            <span className="b">Block &amp; Mortar</span> allows for the
-            grassroots sale of goods and services using the Ethereum blockchain.
+    <div className="home" id="background">
+      <div id="homeText">
+        <div className="f1" id="textSignedIn">
+          <br />
+          <span className="b">Lynx</span> allows for supply chain management using the Ethereum blockchain.
           </div>
-        </div>
-        <div id="homeImg">
-          <img role="presentation" src="/assets/bgimg/phone2.png" />
-        </div>
       </div>
-    ) : (
-      <div className="home" id="background">
-        <div id="homeText">
-          <div id="text" className="f1">
-          Aimed at local communities,<br />
-          <span className="b">Block &amp; Mortar</span> allows for the
-          grassroots sale of goods and services using the Ethereum blockchain.
-          </div>
-
-          <Signup className="modal" />
-        </div>
-        <div id="homeImg">
-          <img role="presentation" src="/assets/bgimg/phone2.png" />
-        </div>
+      <div id="homeImg">
+        <img role="presentation" src="/assets/bgimg/phone2.png" />
       </div>
-    )}
+    </div>
   </div>
 );
 
-const mapState = state => {
+const mapState = ({ user }) => {
   return {
-    isLoggedIn: !!state.currentUser.id
+    isLoggedIn: !!user.id
   };
 };
 

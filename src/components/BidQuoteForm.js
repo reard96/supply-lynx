da2a047607ecdc35ae536ebedb07a122ccca45c7
Alice Luong <aliceluong73@gmail.com>
2018-06-11 21:06:52 -0400

    added stuff

--- src/components/BidQuoteForm.js
+++ src/components/BidQuoteForm.js
@@ -5,33 +5,46 @@ class BidQuoteForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      payment: '',
+      type: '',
       productId: '',
       quantity: '',
       price: '',
       unit: '',
-      class: '',
-      status: '',
       buyer: '',
-      seller: ''
+      seller: '',
+      payment: '',
     }
+    this.onSend = this.onSend(this);
+    this.onCreateBidQuote = this.onCreateBidQuote.bind(this);
   }
 
-  onChange(ev) {
-    
+  onCreateBidQuote(ev) {
+    this.setState({ [ev.target.name]: ev.target.value });
   }
 
-  onUpdate(ev) {
+//Tell Marco that 'class' is reserved word in JS!!!  Using 'type' instead.
+
+  onSend(ev) {
     ev.preventDefault();
+    const { type, productId, quantity, price, unit, buyer, seller, payment } = this.state;
+    type: 'bid' ? (
+      this.props.contract.createBid(this.state)
+      ) : ( this.props.contract.createQuote(this.state) )
   }
 
   render() {
+    const { onSend, onCreateBidQuote } = this;
+    const { productId } = this.state;
     return (
-
+      <div>
+        <h4>Create Bid/Quote</h4>
+        <form onSubmit={ onSendBidQuote }>
+          <label>Product ID</label>
+          <input name = 'Product Id' value = { productId } onChange = { onCreateBidQuote } />
+        </form>
+      </div>
     )
   }
-
-
 }
 
 const mapState = ({ web3, contract, orders}) => {

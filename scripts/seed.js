acc49ac233798ba8990256d61d12e7e8d41e769f
Marco Poetry Chen <marcopchen@gmail.com>
2018-06-11 21:22:26 -0400

    updated seed to include account addresses more easily

--- scripts/seed.js
+++ scripts/seed.js
@@ -1,14 +1,33 @@
-const db = require('../server/db')
-const { User, Service } = require('../server/db/models')
+const db = require('../server/db');
+const { User, Service } = require('../server/db/models');
 
 async function seed() {
-  await db.sync({ force: true })
-  console.log('synced')
+  await db.sync({ force: true });
+  console.log('synced');
+  const addresses = [
+    '0xef57ccc29d110d19d400a62291fda08447a84381',
+    '0x3fba4ff7b740248cbc40b237a147fc65fd1514a5',
+    '0x45825ce9cddd0f0619ee9371f35428a79fc12200',
+    '0xd36a808421aa30dbd621e831f1d78511592ebeca',
+    '0x17eb1cd016d58d81e066a3aafba67ea3328b9990',
+    '0x2046583c34182d6a6d76bddf48e0d21e008aeb44',
+    '0xcf971a483eccfd2d41f05042c18cf3e3612fe7ff',
+    '0x9e70e44103ef95f62e609f12cfaf6c11f88e3314',
+    '0x1e27631326cc22addbdbaeacea9e1d6038570ff6',
+    '0xe9a5403ce8635d96b56573c6b6d4e03849f91c6c'
+  ];
   const users = await Promise.all([
-    User.create({ address: '0xef57ccc29d110d19d400a62291fda08447a84381', name: 'Supply Lynx Inc.', category: 'admin' }),
-    User.create({ address: '0x3fba4ff7b740248cbc40b237a147fc65fd1514a5', name: 'Ultra Market Ltd.', category: 'buyer' }),
-    User.create({ address: '0x45825ce9cddd0f0619ee9371f35428a79fc12200', name: 'Crop Producer Co.', category: 'seller' }),
-  ])
+    User.create({ address: addresses[0], name: 'Supply Lynx Inc.', category: 'admin' }),
+    User.create({ address: addresses[1], name: 'Ultra Market Ltd.', category: 'buyer' }),
+    User.create({ address: addresses[2], name: 'Whole Retail Ltd.', category: 'buyer' }),
+    User.create({ address: addresses[3], name: 'Main Chain Ltd.', category: 'buyer' }),
+    User.create({ address: addresses[4], name: 'Block Mart Ltd.', category: 'buyer' }),
+    User.create({ address: addresses[5], name: 'Make Everything Co.', category: 'seller' }),
+    User.create({ address: addresses[6], name: 'Max Wholesale Co.', category: 'seller' }),
+    User.create({ address: addresses[7], name: 'Super Producer Co.', category: 'seller' }),
+    User.create({ address: addresses[8], name: 'Mega Supplier Co.', category: 'seller' }),
+    User.create({ address: addresses[9], name: 'All Goods Co.', category: 'seller' }),
+  ]);
   const services = await Promise.all([
     Service.create({ name: 'mung bean' }),
     Service.create({ name: 'vanilla' }),
@@ -26,19 +45,19 @@ async function seed() {
     Service.create({ name: 'coconut' }),
     Service.create({ name: 'barley' }),
     Service.create({ name: 'corn' }),
-  ])
+  ]);
 }
 
 seed()
   .catch(err => {
-    console.error(err.message)
-    console.error(err.stack)
-    process.exitCode = 1
+    console.error(err.message);
+    console.error(err.stack);
+    process.exitCode = 1;
   })
   .then(() => {
-    console.log('closing connection')
-    db.close()
-    console.log('connection closed')
-  })
+    console.log('closing connection');
+    db.close();
+    console.log('connection closed');
+  });
 
-console.log('seeding')
+console.log('seeding');

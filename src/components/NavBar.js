335d74a99f5a2525b08239f4c3020da797b5a209
Alice Luong <aliceluong73@gmail.com>
2018-06-14 13:50:45 -0400

    more changes to nav-bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,8 +9,6 @@ import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 
-import FAQ from './FAQ';
-import About from './About';
 
 const styles = {
   root: {
--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,23 +1,13 @@
 import React, { Component } from 'react';
-import { Route, Switch, withRouter } from 'react-router-dom';
-import { connect } from 'react-redux';
-import Homepage from './Homepage';
+import { Route, Switch } from 'react-router-dom';
 import NavBar from './NavBar';
 import FAQ from './FAQ';
 import About from './About';
-import Profile from './Profile';
 import Dashboard from './Dashboard';
-import Header from './Header';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+//import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 class Routes extends Component {
-  componentDidMount() {
-    window.scroll(0, 0);
-  }
-
   render() {
-    const { isLoggedIn } = this.props;
-    console.log('logged in:', isLoggedIn);
     return (
       <div>
         <NavBar />
@@ -25,7 +15,6 @@ class Routes extends Component {
           <Route exact path='/' component={Dashboard} />
           <Route exact path='/faq' component={FAQ} />
           <Route exact path='/about' component={About} />
-          <Route exact path='/profile' component={Profile} />
           {/*<MuiThemeProvider>*/}
             <Route exact path='/dashboard' component={Dashboard} />
           {/*</MuiThemeProvider>*/} 
@@ -35,11 +24,5 @@ class Routes extends Component {
   }
 }
 
-const mapState = ({ user }) => {
-  return {
-    isLoggedIn: !!user.id
-  };
-};
-
-export default withRouter(connect(mapState)(Routes));
+export default Routes;
 
